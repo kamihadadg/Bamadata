f5e4404a39deac25691bbefb3d9dd228932d2675
kamihadadg <83748600+kamihadadg@users.noreply.github.com>
2024-05-20 18:43:31 +0330

    Revert "Result on Html"
    This reverts commit b0d3c000509c3e82ee5751fa50d63e0d3b77c96e.

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
-import webbrowser
-# اجرای توابع برای گرفتن داده‌ها
 from Bama import scrape_car_reviews
 from CarPriceList import scrape_car_price_list
 
@@ -9,39 +6,16 @@ driver_path = '/path/to/chromedriver'
 # Path to cars.json
 cars_json_path = 'cars.json'
 
-# گرفتن داده‌ها از توابع
-df_reviews, pivot_table_reviews = scrape_car_reviews(driver_path, cars_json_path)
-df_prices = scrape_car_price_list()
+df, pivot_table = scrape_car_reviews(driver_path, cars_json_path)
 
-# تبدیل داده‌ها به فرمت HTML
-html_reviews = df_reviews.to_html()
-html_pivot_reviews = pivot_table_reviews.to_html()
-html_prices = df_prices.to_html()
+# چاپ اطلاعات DataFrame
+print(df)
 
-# ادغام کدهای HTML و Python
-html_content = f"""
-<!DOCTYPE html>
-<html>
-<head>
-    <title>نتایج</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-    <h1>Car Reviews</h1>
-    {html_reviews}
-    <h1>Car Reviews Pivot Table</h1>
-    {html_pivot_reviews}
-    <h1>Car Prices</h1>
-    {html_prices}
-</body>
-</html>
-"""
-
-# ذخیره فایل HTML
-with open('results.html', 'w', encoding='utf-8') as f:
-    f.write(html_content)
-
-print("Results saved to 'results.html'")
+# چاپ جدول Pivot
+print(pivot_table)
 
 
-webbrowser.open('results.html')
+df = scrape_car_price_list()
+
+# چاپ نتیجه
+print(df)
