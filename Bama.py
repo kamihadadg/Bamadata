f9bb564a43caa01e941900b6782599175c345651
kamihadadg <83748600+kamihadadg@users.noreply.github.com>
2024-05-20 17:57:34 +0330

    1

--- Bama.py
+++ Bama.py
@@ -3,10 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import json
-import time  # برای استفاده از تابع sleep()
+import time
 
 # Path to your downloaded ChromeDriver
-driver_path = ''
+driver_path = '/path/to/chromedriver'
+
 # ساخت یک نمونه از تنظیمات Chrome
 chrome_options = Options()
 chrome_options.add_argument("--webdriver.chrome.driver=" + driver_path)
@@ -26,16 +27,18 @@ data = []  # لیست برای ذخیره داده‌های جمع‌آوری ش
 
 def get_data(car_name):
     chrome_driver.get(url + '/' + car_name)
-    time.sleep(2)  # وقفه 3 ثانیه
+    time.sleep(2)  # وقفه 2 ثانیه
     progress_data = chrome_driver.find_elements(By.CLASS_NAME, 'car-reviews-base__progress-holder')
     for div in progress_data:
-        elements = div.find_elements(By.CLASS_NAME, 'rounded-progress-wrapper') 
+        elements = div.find_elements(By.CLASS_NAME, 'rounded-progress-wrapper')
         for wrapper in elements:
             percent_div = wrapper.find_elements(By.CLASS_NAME, 'rounded-progress')
             for p in percent_div:
                 label = wrapper.text.split('\n')[1]
                 percentage = p.find_element(By.CLASS_NAME, 'rounded-progress__text').text.strip()
-                data.append({"Car": car_name, "Label": label, "Percentage": percentage})
+                # تبدیل رشته درصد به عدد
+                percentage_value = float(percentage.rstrip('%'))
+                data.append({"Car": car_name, "Label": label, "Percentage": percentage_value})
 
 # برای هر خودرو در لیست، داده‌های مربوطه را بیاورید
 for car_info in cars_list:
@@ -47,15 +50,19 @@ chrome_driver.quit()
 
 # تبدیل لیست داده‌ها به DataFrame
 df = pd.DataFrame(data)
+
+# افزودن ستون SumPercentage
 df['SumPercentage'] = df.groupby('Car')['Percentage'].transform('sum')
+
 # چاپ اطلاعات DataFrame
 print(df)
+
 # ذخیره DataFrame به عنوان یک فایل CSV
 df.to_csv('car_reviews.csv', index=False, encoding='utf-8-sig')
 
 # ساخت جدول Pivot بر اساس نام ماشین و برچسب‌ها
 pivot_table = df.pivot_table(index='Car', columns='Label', values='Percentage', aggfunc='first')
 
+
 # چاپ جدول Pivot
 print(pivot_table)
-
